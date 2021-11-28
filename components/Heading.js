56a11046692f15c7ec8c1694f099d581446cd5ba
jaygal <jaygal@Joshuas-MBP.lan>
2021-11-28 10:58:54 +0100

    updated &apos;

--- components/Heading.js
+++ components/Heading.js
@@ -7,9 +7,9 @@ const Heading = () => {
     <div className={styles.headingContainer}>
       <h1 className={styles.heading}>
         <span className={styles.pink}>
-          goodbye <span className={styles.letterSpacing}>20's</span>
+          goodbye <span className={styles.letterSpacing}>20&apos;s</span>
         </span>{' '}
-        hello <span className={styles.letterSpacing}>30's</span>
+        hello <span className={styles.letterSpacing}>30&apos;s</span>
       </h1>
       <div className={styles.triangle}>
         <Image width={380.67} height={375.01} src="/purple-triangle.svg" />
