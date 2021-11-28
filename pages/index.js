2bf8e5b371b40b989b108657a0282f7760a891cd
jaygal <jaygal@Joshuas-MBP.lan>
2021-11-28 09:02:24 +0100

    updated index

--- pages/index.js
+++ pages/index.js
@@ -25,13 +25,39 @@ export default function Home() {
           crossOrigin=""
         />
       </Head>
-
       <main>
         <Heading />
         <div className="flex">
           <Polaroid />
           <TextBox text="Let's celebrate being old together" />
           <Button text="RSVP" />
+        </div>
+        <div className="shapes-absolute circle-pink">
+          <Image height={116} width={116} src="/circle-pink.svg" />
+        </div>
+        <div className="shapes-absolute circle-purple">
+          <Image height={112} width={112} src="/circle-purple.svg" />
+        </div>
+        <div className="shapes-absolute circle-yellow">
+          <Image height={192} width={192} src="/circle-yellow.svg" />
+        </div>
+        <div className="shapes-absolute crown-turquoise">
+          <Image height={51} width={60} src="/crown-turquoise.svg" />
+        </div>
+        <div className="shapes-absolute crown-yellow">
+          <Image height={26} width={30} src="/crown-yellow.svg" />
+        </div>
+        <div className="shapes-absolute ring-orange">
+          <Image height={24} width={24} src="/ring-orange.svg" />
+        </div>
+        <div className="shapes-absolute ring-pink">
+          <Image height={24} width={24} src="/ring-pink.svg" />
+        </div>
+        <div className="shapes-absolute ring-yellow">
+          <Image height={24} width={24} src="/ring-yellow.svg" />
+        </div>
+        <div className="shapes-absolute wiggle">
+          <Image height={163} width={170} src="/wiggle.svg" />
         </div>
       </main>
     </div>
